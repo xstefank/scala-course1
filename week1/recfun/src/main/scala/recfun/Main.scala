0a281803a7245e8c2f817d6725acc8692dc8e5b8
xstefank <mjolnir122@gmail.com>
2016-07-10 17:25:07 +0200

    week1 recfun assingment final adjustments

--- week1/recfun/src/main/scala/recfun/Main.scala
+++ week1/recfun/src/main/scala/recfun/Main.scala
@@ -14,47 +14,35 @@ object Main {
    * Exercise 1
    */
     def pascal(c: Int, r: Int): Int = {
-      def factorial(n: Int): Int = {
-        def loop(acc: Int, n: Int): Int =
-          if (n == 0) acc
-          else loop(acc * n, n - 1);
-        loop(1, n)
+      if (c == 0 || c == r) {
+        1
+      }else {
+        pascal(c - 1, r - 1) + pascal(c, r - 1)
       }
-
-      factorial(r) / (factorial(r - c) * factorial(c))
     }
   
   /**
    * Exercise 2
    */
     def balance(chars: List[Char]): Boolean = {
-      import scala.collection.mutable.Stack
 
-      def processChar(parenthesses: Stack[Char], string: List[Char]): Boolean = {
-        //if we are on the end on the string the stack must be empty
+      def processChar(parenthesses: Int, string: List[Char]): Boolean = {
         if (string.isEmpty) {
-          parenthesses.isEmpty
+          parenthesses == 0
         } else {
-
-          //else process the first character
-          //if it is '(' add it to stack
-          //if it is ')' remove one element from the stack
-          val character = string.head;
-          if (character.equals('(')) {
-            parenthesses.push('(');
-          } else if (character.equals(')')) {
-            //if the stack is empty there is no matching '('
-            if (parenthesses.isEmpty) {
-              return false
-            } else {
-              parenthesses.pop();
-            }
+          val character = string.head
+          val change = {
+            if (character == '(') parenthesses + 1
+            else if (character == ')') parenthesses - 1
+            else parenthesses
           }
-          processChar(parenthesses, string.tail);
+
+          if (parenthesses >= 0) processChar(change, string.tail)
+          else false
         }
       }
 
-      processChar(Stack[Char](), chars)
+      processChar(0, chars)
     }
   
   /**
@@ -62,10 +50,8 @@ object Main {
    */
     def countChange(money: Int, coins: List[Int]): Int = {
       if (money == 0) {
-        //we made the exact number from coins - valid option
         1
       } else if (!coins.isEmpty && money > 0) {
-        //still have some money and some coins left
         countChange(money - coins.head, coins) + countChange(money, coins.tail)
       } else {
         0
