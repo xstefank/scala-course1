2dcf249667b5b6a1bbf14cadf3688b4761ab8b69
xstefank <mjolnir122@gmail.com>
2016-07-14 20:28:08 +0200

    week3 lecture

---
+++ week3/lecture3/build.sbt
@@ -0,0 +1,6 @@
+name := "lecture3"
+
+version := "1.0"
+
+scalaVersion := "2.11.8"
+    
---
+++ week3/lecture3/src/main/scala/week3/Hello.scala
@@ -0,0 +1,10 @@
+package week3
+
+/**
+  * @author <a href="mailto:stefankomartin6@gmail.com">Martin Štefanko</a>
+  */
+object Hello {
+
+  def main(args: Array[String]) = println("Hello, world!")
+
+}
